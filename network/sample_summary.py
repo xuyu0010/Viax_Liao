ff99c093e55f773ae2bc5d3e7d7ea1893994504b
xuyu0010 <xuyu0014@e.ntu.edu.sg>
2020-04-19 15:28:50 +0800

    Update sample_summary.py

--- network/sample_summary.py
+++ network/sample_summary.py
@@ -2,10 +2,11 @@ import torch
 from thop import profile
 from thop import clever_format
 from torchsummary import summary
+from .mfnet_sp_linear_5pixel_meth2_b import MFNET_FIVEP_LINEAR_FRAME
 
 # logging.getLogger().setLevel(logging.DEBUG)
 # ---------
-net = MFNET_SP_LINEAR_REGION(num_classes=100, pretrained=False)
+net = MFNET_FIVEP_LINEAR_FRAME(num_classes=100, pretrained=False)
 data = torch.autograd.Variable(torch.randn(1,3,16,224,224))
 flops, params = profile(net, inputs=(data, ))
 # summary(net, (3,16,224,224))
